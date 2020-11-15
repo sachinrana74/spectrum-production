8211ca496680eaa2194cec00a5811a5c00cf702c
SACHIN RANA <45383591+sachinrana74@users.noreply.github.com>
2020-11-15 23:32:53 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+        <div class="copy-right_text">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-7 col-md-8">
+                    <p class="copy_right ">
+                        Copyright © <script>document.write(new Date().getFullYear());</script> All rights reserved <span style="text-decoration-line: underline">Spectrum</span>
+                    </div>
+                    <div class="col-xl-5 col-md-4">
+                        <div class="socail_links">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <i class="ti-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="ti-twitter-alt"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </footer>
